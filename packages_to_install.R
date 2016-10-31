e5a068db201458f54b480d2d3c2c57a4fea18512
dawkiny@gmail.com <dawkiny@gmail.com>
2016-10-31 22:18:04 +0900

    The list of packages to install

---
+++ packages_to_install.R
@@ -0,0 +1,107 @@
+#START OF LIST
+
+# Data type ------------
+install.packages('zoo')
+install.packages('xfs')
+install.packages('stringr')
+install.packages('timeDate')
+install.packages('lubricate')
+
+# Data Structure ------------
+install.packages('data.table')
+install.packages('rjson')
+install.packages('treemap')
+
+# Data Manipulation ------------
+install.packages('plyr')
+install.packages('dplyr')
+install.packages('reshape')
+install.packages('reshape2')
+install.packages('tidyr')# evolution of reshape2
+install.packages('FactoMineR')# Multivariate Exploratory data analysis methods such as principal component methods and clustering.
+
+# SQL ------------
+install.packages('sqldf')
+install.packages('RPostgreSQL')
+install.packages('RMySQL')
+install.packages('RMongo')
+install.packages('RODBC')
+install.packages('RJDBC')
+install.packages('RSQLite')
+install.packages('pandasql')
+
+# Web applications ------------
+install.packages('httr')# Work with URL, HTTP
+install.packages('shiny')
+install.packages('RCurl')# HTTP, URI
+
+# Statistics ------------
+
+## Basic
+install.packages('car')# Companion to Applied Regression
+
+## Machine Learning Algorithm
+install.packages('party')
+install.packages('rpart')
+install.packages('caret')
+install.packages('survival')
+install.packages('glmnet')
+install.packages('randomForest')
+install.packages('e1071')
+install.packages('lars')
+install.packages('algae')
+install.packages('arules')
+install.packages('tree')
+install.packages('mboost')# Gradient Descent Algorithm(boosting)
+install.packages('gbm')# Generalised Boosted Models
+install.packages('ROCR')# Visualising the Performance of Scring Classifiers
+install.packages('forecast')# Forcasting Func. for Timeseries & Linear Models
+install.packages('klaR')# Classification & Visualisation
+install.packages('lme4')# Linear Mixed-Effects Models using 'Eigen' and S4
+install.packages('nlme')# Linear and Nonlinear Mixed Effects Models
+install.packages('multcomp')# Simultaneous Inference in General Parametric Models
+install.packages('quantmod')# Quantitative Financial Modelling Framework
+install.packages('kernlab')# Kernel-Based Machine Learning Lab
+
+## Neural Networks
+install.packages('nnet')
+
+# Visualisation ------------
+
+## Graph
+install.packages('ggplot2')
+install.packages('riverplot')
+install.packages('dygraphs')
+install.packages('threeJS')
+install.packages('plotly')
+install.packages('igraph')
+install.packages('ggvis')
+install.packages('vcd')#Structure plot(Categorical data)
+install.packages('manipulate')# Interactive plotting
+install.packages('gridExtra')# Grid Graphics
+
+## Color
+install.packages('RcolorBrewer')
+install.pakcages('colorRampPalette')
+install.packages('colorspace')
+
+## Map
+install.packages('scales')
+
+# Programming ------------
+install.packages('devtools')
+install.packages('RcppArmadillo')
+install.packages('digest')# hash digest
+install.packages('roxygen2')# R documentation system
+install.packages('foreach')
+
+# Misc ------------
+install.packages('rmarkdown')
+install.packages('knitr')
+install.packages('brew')
+install.packages('nutshell')# datasets of 'R in nutshell'
+
+##?? ------------
+install.packages('mgcv')# Mixed GAM Computation Vehicle with GCV/AIC/REML Smoothness Estimation
+install.packages('qcc')# Quality Control Charts: an R package for quality control charting and statistical process control.
+#END OF LIST
